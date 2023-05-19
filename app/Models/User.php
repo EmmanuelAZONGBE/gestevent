425000a5eef0a46361a11d017caa367f3fbcbc5c
EmmanuelAZONGBE <jazongbe25@gmail.com>
2023-05-19 15:59:57 +0200

    firs

---
+++ app/Models/User.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Organisateur;
+use App\Models\Prestataire;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Client;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'picture',
+        'email',
+        'adresse',
+        'password',
+        
+    ];
+
+    public function client()
+
+    {
+        return $this ->belongsTo(Client::class);
+    }
+
+    public function organisateur()
+
+    {
+        return $this ->belongsTo(Organisateur::class);
+    }
+
+    public function prestataire()
+
+    {
+        return $this ->belongsTo(Prestataire::class);
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+    
+}
